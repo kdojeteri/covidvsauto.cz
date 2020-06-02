cd58657bd6f7a7e3401fcd806037d34b488644b4
Teri Hornych <hornych@engeto.com>
2020-06-02 13:22:28 +0200

    use modules, create skeleton files for downloading data

---
+++ src/sources/data.mjs
@@ -0,0 +1,10 @@
+export const data = {
+    zraneni: {
+        
+    },
+    datum_zraneni: new Date(),
+    covid: {
+
+    },
+    datum_covid: new Date(),
+}
---
+++ src/sources/zraneni.mjs
@@ -0,0 +1,6 @@
+import {data} from './data';
+import fetch from 'node-fetch';
+
+export async function aktualizuj() {
+
+}
